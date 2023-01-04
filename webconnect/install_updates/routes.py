2b5db4092f29a195381911b671292de039f97f0f
Devopcasting <devopcasting@gmail.com>
2023-01-04 12:05:45 +0530

    updated install-updated blueprint obj

--- webconnect/install_updates/routes.py
+++ webconnect/install_updates/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template,url_for,flash,request
 import subprocess
+import json
 
 # Blueprint object
 installupdate_blue_obj = Blueprint('install_updates',__name__,template_folder='templates')
@@ -8,5 +9,25 @@ installupdate_blue_obj = Blueprint('install_updates',__name__,template_folder='t
 @installupdate_blue_obj.route('/install-update',methods=['GET','POST'])
 def install_update():
     # Check the installed version of Google Chrome
+    check_google_installed_cmd = ["which","google-chrome-stable"]
+    check_google_installed_ver_cmd = ["google-chrome-stable","--version"]
+    google_output = ""
+    proc1 = subprocess.Popen(check_google_installed_cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    
+    if len(str(proc1.communicate()[0],'UTF-8')) == 0:
+        google_output = "nop"
+    else:
+        proc2 = subprocess.Popen(check_google_installed_ver_cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        google_output = str(proc2.communicate()[0],'UTF-8')
 
-    return render_template('install_updates/install_updates.html')
+    # Check the installed version of FreeRDP
+    check_freerdp_installed_cmd = ["which","xfreerdp"]
+    check_freerdp_installed_ver_cmd = 'xfreerdp --version'
+    freerdp_output = ""
+    proc3 = subprocess.Popen(check_freerdp_installed_cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    if len(str(proc3.communicate()[0],'UTF-8')) == 0:
+        freerdp_output = "nop"
+    else:
+        proc4 = subprocess.Popen(check_freerdp_installed_ver_cmd,shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        freerdp_output = str(proc4.communicate()[0],'UTF-8')
+    return render_template('install_updates/install_updates.html',is_google=google_output,is_freerdp=freerdp_output)
